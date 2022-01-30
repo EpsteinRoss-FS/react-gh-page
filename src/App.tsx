fd69441e5f5704e6aa6dbf2ad4764c0525ffa0fd
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-01-30 11:28:52 -0500

    Added initial requirements and files, configured tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavComponent from './components/NavComponent';
 
 function App() {
   return (
     <div className="App">
+      <NavComponent />
+      <section>
+        <Routes>
+          {/*<Route path="/" element={<Profile />} />*/}
+        </Routes>
+      </section>
 
     </div>
   );
