2f11295b9a18e182aebdfca31a4c4858959ba17a
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-10 10:56:09 -0500

    all navbar links work and render the respective pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import NavComponent from './components/Navbar/NavComponent';
 import HomeComponent from "./components/Homepage/HomeComponent";
 import MediaComponent from "./components/Media/MediaComponent";
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import SongsComponent from "./components/Songs/SongsComponent";
+import PressKitComponent from "./components/PressKit/PressKitComponent";
+import GalleryComponent from "./components/Gallery/GalleryComponent";
+import EventsComponent from "./components/Events/EventsComponent";
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
             <Route path="/" element={<HomeComponent />} />
             <Route path="home" element={<HomeComponent />} />
             <Route path="media" element={<MediaComponent />} />
+            <Route path="events" element={<EventsComponent />} />
+            <Route path="songs" element={<SongsComponent />} />
+            <Route path="press-kit" element={<PressKitComponent />} />
+            <Route path="gallery" element={<GalleryComponent />} />
         </Routes>
       </section>
 
