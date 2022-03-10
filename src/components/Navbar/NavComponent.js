31f004351b6439b7c00a929af8275cd7264573f9
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-09 20:56:07 -0500

    added ringbearer font

--- src/components/Navbar/NavComponent.js
+++ src/components/Navbar/NavComponent.js
@@ -25,7 +25,7 @@ export default function NavComponent() {
                                  <h1 class='lukuHeader'>Luku The Mad Skald</h1>
                                 </div>
                                 <div className="hidden sm:block sm:ml-6">
-                                    <div className="flex space-x-4">
+                                    <div className="flex mx-auto space-x-4">
                                         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                                         <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
                                             Home
