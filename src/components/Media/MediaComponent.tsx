dfb5035453ffdff61212900b4f3f800e60aed072
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-12 23:44:02 -0500

    added spotify and apple music feeds

--- src/components/Media/MediaComponent.tsx
+++ src/components/Media/MediaComponent.tsx
@@ -1,9 +1,46 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-
-
+import SpotifyPlayer from 'react-spotify-web-playback';
+import Feed from "instagram-feed-embed";
+import { FacebookProvider, Like,EmbeddedPost, Page  } from 'react-facebook';
+import ragnatree from '../../assets/images/ragnatree.png';
+import Embed from 'react-music-embed';
 
 export default function MediaComponent() {
-    return(<h1>MEDIA WORKS</h1>)
+    return (
+
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+
+            <div className="absolute inset-0">
+                <div className="bg-white h-1/3 sm:h-2/3" />
+            </div>
+            <div className="relative max-w-7xl mx-auto">
+                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                    <div className="flex flex-col rounded-lg m-lg-auto  overflow-hidden">
+                        <FacebookProvider appId="1389193744849483">
+                            <div className="flex-shrink-0">
+                                <h1 className="text-center">Spotify</h1>
+                                <iframe src="https://open.spotify.com/embed/artist/6mbwmUYgWsO2IVS6lMTmQC" width="320"
+                                        height="400" frameBorder="0" allowTransparency={true}
+                                        allow="encrypted-media"></iframe>
+                            </div>
+                        </FacebookProvider>
+                    </div>
+                    <div className="flex flex-col rounded-lg  overflow-hidden">
+                        {/*<img src={ragnatree}/>*/}
+                    </div>
+                    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                        {/*<FacebookProvider appId="1389193744849483">*/}
+                        <div className="flex-shrink-0">
+                            <script async src="https://www.instagram.com/embed.js" />
+                            <Embed url="https://music.apple.com/us/album/were-all-going-to-helheim/1503931963" />
+                            <Embed url="https://music.apple.com/us/album/pissed-drunk-pissed-off/1563884170" />
+                            <Embed url="https://music.apple.com/us/album/from-under-the-northern-lights/1503456053" />
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
