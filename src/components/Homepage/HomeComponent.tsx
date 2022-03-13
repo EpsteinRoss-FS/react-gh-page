b0d3e359d0b5e4bb0b2ee080550ff703385d4072
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-12 19:18:48 -0500

    removed uneeded posts variable from home component

--- src/components/Homepage/HomeComponent.tsx
+++ src/components/Homepage/HomeComponent.tsx
@@ -2,64 +2,6 @@ import Feed from "instagram-feed-embed";
 import { FacebookProvider, Like,EmbeddedPost, Page  } from 'react-facebook';
 import ragnatree from '../../assets/images/ragnatree.png';
 
-/* This example requires Tailwind CSS v2.0+ */
-const posts = [
-    {
-        title: 'Boost your conversion rate',
-        href: '#',
-        category: { name: 'Article', href: '#' },
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-        readingTime: '6 min',
-        author: {
-            name: 'Roel Aufderehar',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        title: 'How to use search engine optimization to drive sales',
-        href: '#',
-        category: { name: 'Video', href: '#' },
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
-        date: 'Mar 10, 2020',
-        datetime: '2020-03-10',
-        imageUrl:
-            'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-        readingTime: '4 min',
-        author: {
-            name: 'Brenna Goyette',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        title: 'Improve your customer experience',
-        href: '#',
-        category: { name: 'Case Study', href: '#' },
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-        date: 'Feb 12, 2020',
-        datetime: '2020-02-12',
-        imageUrl:
-            'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-        readingTime: '11 min',
-        author: {
-            name: 'Daniela Metz',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-]
-
 export default function HomeComponent() {
     return (
 
@@ -84,13 +26,7 @@ export default function HomeComponent() {
                         </FacebookProvider>
                     </div>
                     <div className="flex flex-col rounded-lg  overflow-hidden">
-
                         <img src={ragnatree}/>
-                        {/*<FacebookProvider appId="1389193744849483">*/}
-                        {/*    <div className="flex-shrink-0">*/}
-                        {/*        <Page href="https://www.facebook.com/lukuthemad" tabs="timeline" />*/}
-                        {/*    </div>*/}
-                        {/*</FacebookProvider>*/}
                     </div>
                     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
                         {/*<FacebookProvider appId="1389193744849483">*/}
