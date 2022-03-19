a40b84641b85030aceddd1f8a038abca7a77bc76
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-18 21:09:01 -0400

    added pupblic folder and basic gallery

--- src/components/Gallery/GalleryComponent.tsx
+++ src/components/Gallery/GalleryComponent.tsx
@@ -1,9 +1,17 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
+import ImageGallery from 'react-image-gallery';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 
-
+const images = [
+    {
+        original: "/images/ragnatree.png",
+        thumbnail: '/images/ragnatree.png',
+    },
+]
 export default function GalleryComponent() {
-    return(<h1>GALLERY WORKS</h1>)
+    return(
+        <ImageGallery items={images} />
+    )
 }
