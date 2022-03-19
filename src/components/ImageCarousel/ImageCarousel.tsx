8c6f09a9687dccbe91538c463ca9406f1dd5cc8e
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-03-19 11:49:37 -0400

    mostly works, testing thumbs

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,76 +1,72 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import ImageGallery from 'react-image-gallery';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import {map} from "react-bootstrap/ElementChildren";
 import {Link, useLocation, useSearchParams} from "react-router-dom";
-import {ImageViewer} from "react-image-viewer-dv";
+import "react-awesome-lightbox/build/style.css";
+import DztImageGalleryComponent from "reactjs-image-gallery";
 
 
 
 export default function ImageCarousel() {
 
 let khrf2021 = [
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_1.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_2.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_3.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_4.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_5.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_6.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_7.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_8.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_9.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_10.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_11.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_12.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_13.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_14.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_15.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_16.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_17.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_18.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_19.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_20.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_21.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_22.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_23.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_24.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_25.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_26.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_27.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_28.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_29.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_30.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_31.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_32.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_33.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_34.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_35.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_36.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_37.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_38.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_39.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_40.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_41.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_42.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_43.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_44.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_45.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_46.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_47.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_48.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_49.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_50.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_51.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_52.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_53.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_54.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_55.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_56.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_57.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_58.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_59.jpg'},
-        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_60.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_1.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_1.jpg',},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_2.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_2.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_3.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_3.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_4.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_4.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_5.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_5.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_6.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_6.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_7.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_7.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_8.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_8.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_9.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_9.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_10.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_10.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_11.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_11.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_12.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_12.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_13.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_13.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_14.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_14.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_15.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_15.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_16.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_16.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_17.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_17.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_18.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_18.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_19.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_19.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_20.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_20.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_21.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_21.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_22.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_22.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_23.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_23.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_24.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_24.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_25.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_25.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_26.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_26.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_27.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_27.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_28.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_28.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_29.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_29.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_30.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_30.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_31.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_31.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_32.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_32.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_33.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_33.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_34.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_34.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_35.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_35.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_36.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_36.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_37.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_37.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_38.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_38.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_39.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_39.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_40.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_40.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_41.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_41.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_42.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_42.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_43.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_43.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_44.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_44.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_45.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_45.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_46.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_46.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_47.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_47.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_48.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_48.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_49.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_49.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_50.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_50.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_51.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_51.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_52.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_52.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_53.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_53.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_54.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_54.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_55.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_55.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_56.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_56.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_57.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_57.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_58.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_58.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_59.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_59.jpg'},
+        {url: '/images/galleries/khrf2021/Luku_Khrf_2021_60.jpg', thumbUrl: '/images/galleries/khrf2021/Luku_Khrf_2021_60.jpg'},
         // {url: '/images/galleries/khrf2021/Luku_and_Ulfhedinn_Immigrant_Song.mp4'},
 
     ];
@@ -95,7 +91,7 @@ let irf2019 = [
         {url: '/images/galleries/irf2019/irf19_luku4.jpg'},
         ];
 
-let khrf19 = [
+let khrf2019 = [
         {url: '/images/galleries/khrf2019/luku1_khrf19.jpg'},
         {url: '/images/galleries/khrf2019/luku2_khrf19.jpg'},
         {url: '/images/galleries/khrf2019/luku3_khrf19.jpg'},
@@ -130,7 +126,7 @@ let khrf19 = [
 
     ];
 
-let khrf20 = [
+let khrf2020 = [
         {url: '/images/galleries/khrf2020/khrf2020_1.jpg'},
         {url: '/images/galleries/khrf2020/khrf2020_2.jpg'},
         {url: '/images/galleries/khrf2020/khrf2020_3.jpg'},
@@ -199,16 +195,16 @@ let khrf20 = [
 
         const test = [
                 {
-                        original: 'https://picsum.photos/id/1018/1000/600/',
-                        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+                        url: 'https://picsum.photos/id/1018/1000/600/',
+                        thumbUrl: 'https://picsum.photos/id/1018/250/150/',
                 },
                 {
-                        original: 'https://picsum.photos/id/1015/1000/600/',
-                        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+                        url: 'https://picsum.photos/id/1015/1000/600/',
+                        thumbUrl: 'https://picsum.photos/id/1015/250/150/',
                 },
                 {
-                        original: 'https://picsum.photos/id/1019/1000/600/',
-                        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+                        url: 'https://picsum.photos/id/1019/1000/600/',
+                        thumbUrl: 'https://picsum.photos/id/1019/250/150/',
                 },
         ];
 
@@ -225,9 +221,28 @@ let khrf20 = [
                 console.log(galleryImages);
         }
 
+        switch(galleryId){
+                case 'irf2019':
+                        galleryImages = test;
+                        break;
+                case 'khrf2020':
+                        galleryImages = khrf2020;
+                        break;
+                case 'khrf2021':
+                        galleryImages = khrf2021;
+                        break;
+                case 'khrf2019':
+                        galleryImages = khrf2019;
+                        break;
+                case 'robinct2021':
+                        galleryImages = rh2021;
+                        break;
+        }
+
         const galleryName =searchParams.get('galleryName')
     // console.log(searchParams);
         console.log(galleryImages);
+
 
     return(
 
@@ -235,53 +250,19 @@ let khrf20 = [
 
                 <section className="overflow-hidden text-gray-700 ">
                         <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-                                <div className="flex flex-wrap -m-1 md:-m-2">
-                                        {galleryImages.map((img) =>
-                                        <div className="flex flex-wrap w-1/3">
-                                                <div className="w-50 p-1 md:p-2">
-                                                        <img alt="gallery"
-                                                             className="block object-cover object-center w-25 h-full rounded-lg"
-                                                             src={img.url} />
-                                                </div>
-                                        </div>)}
-                                </div>
+                                <DztImageGalleryComponent images={galleryImages} />
+                                {/*<div className="flex flex-wrap -m-1 md:-m-2">*/}
+                                {/*        {galleryImages.map((img) =>*/}
+                                {/*        <div className="flex flex-wrap w-1/3">*/}
+                                {/*                <div className="w-50 p-1 md:p-2">*/}
+                                {/*                        <img alt="gallery"*/}
+                                {/*                             className="block object-cover object-center w-25 h-25 rounded-lg"*/}
+                                {/*                             src={img.url} />*/}
+                                {/*                </div>*/}
+                                {/*        </div>)}*/}
+                                {/*</div>*/}
                         </div>
                 </section>
-            {/*<div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">*/}
-            {/*    <div className="space-y-12">*/}
-            {/*        <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">*/}
-            {/*            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">{galleryName} Gallery</h2>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-            {/*            /!*{galleryImages.map((img) => (*!/*/}
-            {/*            /!*        <ImageViewer>*!/*/}
-            {/*            /!*                <img src={img.url}  alt="Your image" />*!/*/}
-            {/*            /!*        </ImageViewer>*!/*/}
-            {/*            /!*))}*!/*/}
-
-            {/*    <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">*/}
-
-            {/*            {galleryImages.map((img) => (*/}
-            {/*                <li  className="relative">*/}
-            {/*                        <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">*/}
-
-            {/*                                        <img src={img.url} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />*/}
-
-            {/*                                <button type="button" className="absolute inset-0 focus:outline-none">*/}
-
-            {/*                                                /!*<img src={img.url}  alt="Your image" />*!/*/}
-
-            {/*                                </button>*/}
-
-            {/*                        </div>*/}
-            {/*                        /!*<p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{file.title}</p>*!/*/}
-            {/*                        /!*<p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.size}</p>*!/*/}
-            {/*                </li>*/}
-            {/*            ))}*/}
-            {/*    </ul>*/}
-
         </div>
     )
 }
